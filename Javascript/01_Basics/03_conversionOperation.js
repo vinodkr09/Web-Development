997addbdec7d2396c4711f56b42f12722cae2a7f
vinodkr09 <vinod.krmahi@gmail.com>
2023-12-10 04:58:07 +0530

    more operations added

--- Javascript/01_Basics/03_conversionOperation.js
+++ Javascript/01_Basics/03_conversionOperation.js
@@ -20,5 +20,42 @@ console.log(booleanIsLoggedIn);
 
 let stringName = 33
 let stringNumber = String(stringName)
-console.log(stringName);
+console.log(stringNumber);
 console.log(typeof stringNumber)
+
+
+
+//    **************************OPERATIONS*************************************
+let value =3
+let negvalue = -value
+console.log(negvalue);
+
+console.log(2+2)
+console.log(2-2)
+console.log(2*2)
+console.log(2**3)
+console.log(2/3)
+console.log(2%3)
+
+// String
+let str1 = "Saket"
+let str2 = " Anand"
+let str3 = str1 + str2
+console.log(str3)
+
+
+console.log("1" + 2)        // Agar string phele hai to sara value as a string treat hoga
+console.log(1 + "2")        // Agar string last me tb phele normal jo evalutation hai jo jayega phir baad me string ka hoga
+console.log("2"+2+2)        // Agar string phele hai to sara value as a string treat hoga
+console.log(1 + 2 + "2")     // Agar string last me tb phele normal jo evalutation hai jo jayega phir baad me string ka hoga 
+console.log((3+4) * 5 % 3);    // best way to write in brackets
+
+
+console.log(+true)  // It gives output as 1
+console.log(+"")   //It Gives output as 0
+
+
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);   // output will be 101
