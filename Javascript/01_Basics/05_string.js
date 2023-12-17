9e2d390da461ab46aca3ce497d401ffaeaba4812
vinodkr09 <vinod.krmahi@gmail.com>
2023-12-17 06:47:55 +0530

    string modiefied

--- Javascript/01_Basics/05_string.js
+++ Javascript/01_Basics/05_string.js
@@ -1,1 +1,9 @@
-gxrthrxhrtgr
+const name = "Saket Anand"
+const repocount = 9
+console.log(name + repocount); // output Saket Anand9
+console.log(`Hello my name is ${name} and my repo count is ${repocount}`);
+// output is Hello my name is Saket Anand and my repo count is 9
+
+
+
+
